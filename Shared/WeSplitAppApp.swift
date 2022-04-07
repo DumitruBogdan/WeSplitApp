5dae1f89b347b141f7ca7ec6c82b5b490ccd6d68
DumitruBogdan <90133050+DumitruBogdan@users.noreply.github.com>
2022-04-07 10:40:28 +0300

    Initial Commit

---
+++ Shared/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  Shared
+//
+//  Created by Freak on 07.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
